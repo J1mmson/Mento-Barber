845e49393425fffd97472fe6217b4ba9e1936551
Kacper Hojnor <kacper166h@gmail.com>
2026-04-23 00:01:52 +0200

    update

--- src/components/Barber9.jsx
+++ src/components/Barber9.jsx
@@ -123,7 +123,7 @@ const Barber9 = ({ slideInFromRight, fadeInFromLeft }) => {
                     loading="lazy"
                     
                     className="absolute w-[800px] max-w-none z-10"
-            style={{ right: "-80px", top: "20px" }}
+            style={{ right: "-50px", top: "100px" }}
 />
 					<img
 						src={Gradient}
